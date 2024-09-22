1cc98d6948feeb903abed1bf4a9dde54f0df5300
sutanukaa <sutanukachakraborty004@gmail.com>
2024-09-22 15:09:38 +0530

    land

---
+++ src/app/land/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from './Hero'
+import Cards from './cards'
+import Screenshot from './screens'
+
+const page = () => {
+  return (
+
+    <div>
+      <Hero />
+      <Cards />
+      <Screenshot />
+    </div>
+  )
+}
+
+export default page
