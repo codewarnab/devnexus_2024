25c738b95d532b20a2b77123d4f422e35e8d8797
codewarnab <dp950810@gmail.com>
2024-09-22 13:04:09 +0530

    er fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 "use client";
 import "jsvectormap/dist/css/jsvectormap.css";
 import "flatpickr/dist/flatpickr.min.css";
-import "@/css/satoshi.css";
-import "@/css/style.css";
+import "../css/satoshi.css";
+import "../css/style.css";
 import React, { useEffect, useState } from "react";
 import Loader from "../components/common/Loader";
 import Background from "../components/Bg";
@@ -25,16 +25,16 @@ export default function RootLayout({
   }, []);
 
   return (
-  
-   
+
+
     <html lang="en">
       <body suppressHydrationWarning={true}>
         <Background />
-           
+
         {loading ? <Loader /> : children}
-        
+
       </body>
     </html>
- 
+
   );
 }
